bbf2d4511cdb1916729c9934be1540fb32176bf2
ankush4gupta <87088148+ankush4gupta@users.noreply.github.com>
2022-06-06 18:12:06 +0530

    added filtering part ui

--- Reversion/Frontend-backend/frontend/src/components/Product.jsx
+++ Reversion/Frontend-backend/frontend/src/components/Product.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import "./product.css"
 
 export const Product = () => {
     const [data, setdata] = useState([]);
@@ -9,13 +10,93 @@ export const Product = () => {
         try {
             let res = await fetch(`http://localhost:3008/product`)
             let result = await res.json();
-            setdata(result);
+            setdata(result.product);
         } catch (error) {
             console.log(error.message)
         }
     }
+    const handlebrand = (e, value) => {
+        if (value == "brand") {
+            if (e.target.checked) {
+                setbrand([...brand, e.target.value])
+            } else {
+                console.log(e.target.value)
+                const result = brand.filter(word => word !== e.target.value);
+                setbrand(result)
+            }
+        }
+        else if (value == "color") {
+            if (e.target.checked) {
+                setcolor([...color, e.target.value])
+            } else {
+                console.log(e.target.value)
+                const result = color.filter(word => word !== e.target.value);
+                setcolor(result)
+            }
+        } else if (value == "cate") {
+            if (e.target.checked) {
+                setcate([...cate, e.target.value])
+            } else {
+                console.log(e.target.value)
+                const result = cate.filter(word => word !== e.target.value);
+                setcate(result)
+            }
+        }
+
+    }
     console.log(data)
-    return <div>
-        hello
-    </div>
+   return <div>
+       <div className="mainContainer">
+           <div className="filter_container">
+               <div>
+                   <h3>BRANDS NAME</h3>
+                   <ul className="brands">
+                       <li><input onChange={(e) => handlebrand(e, "brand")} type="checkbox" value="Nike" /> Nike</li>
+                       <li><input onChange={(e) => handlebrand(e, "brand")} type="checkbox" value="NETPLAY" /> NETPLAY</li>
+                       <li><input onChange={(e) => handlebrand(e, "brand")} type="checkbox" value="LEVIS" /> LEVIS</li>
+                       <li><input onChange={(e) => handlebrand(e, "brand")} type="checkbox" value="Mpl Sports" /> Mpl Sports</li>
+                       <li><input onChange={(e) => handlebrand(e, "brand")} type="checkbox" value="ADIDAS" /> ADIDAS</li>
+                   </ul>
+               </div>
+               <div>
+                   <h3>CATEGORY</h3>
+                   <ul className="category">
+                       <li><input onChange={(e) => handlebrand(e, "cate")} type="checkbox" value="T-Shirt" /> T-Shirt</li>
+                       <li><input onChange={(e) => handlebrand(e, "cate")} type="checkbox" value="Jacket" /> Jacket</li>
+                       <li><input onChange={(e) => handlebrand(e, "cate")} type="checkbox" value="pant" /> pant</li>
+                       <li><input onChange={(e) => handlebrand(e, "cate")} type="checkbox" value="Shorts" /> Shorts</li>
+                   </ul>
+               </div>
+               <div>
+                   <h3>COLORS</h3>
+                   <ul className="colors">
+                       <li><input onChange={(e) => handlebrand(e, "color")} type="checkbox" value="Blue" /> Blue</li>
+                       <li><input onChange={(e) => handlebrand(e, "color")} type="checkbox" value="Brown" /> Brown</li>
+                       <li><input onChange={(e) => handlebrand(e, "color")} type="checkbox" value="Green" /> Green</li>
+                       <li><input onChange={(e) => handlebrand(e, "color")} type="checkbox" value="Black" /> Black</li>
+                       <li><input onChange={(e) => handlebrand(e, "color")} type="checkbox" value="White" /> White</li>
+                   </ul>
+               </div>
+
+           </div>
+           <div className="Container">
+               {
+                   data.map((e) => {
+                       return <div className="product-card">
+                           <img src={e.image.img1} alt="" />
+                           <p>{e.name}</p>
+                           <p>{e.details}</p>
+                           <div>
+                               <p>Rs.{e.discount_price}</p>
+                               <p>Rs.{e.main_price}</p>
+                               <p>({e.discount}.% off)</p>
+                           </div>
+                           <p>Offer Price : Rs.{e.offer_price}</p>
+                       </div>
+                   })
+               }
+           </div>
+       </div>
+    
+   </div>
 }
