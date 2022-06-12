60bbbe33910ddd3c00fc2be759026d4308e289e1
ankush4gupta <87088148+ankush4gupta@users.noreply.github.com>
2022-06-12 23:50:51 +0530

    fetch data for single user

--- Reversion/Ecommerce_Schema/vite-project/src/components/User/UserDetails.jsx
+++ Reversion/Ecommerce_Schema/vite-project/src/components/User/UserDetails.jsx
@@ -1,6 +1,17 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 export const UserDetails = ()=>{
-    let {id}  =  useParams()
+    let {id}  =  useParams();
+    const [singleuser, setsingleuser] = useState();
+    useEffect(()=>{
+        fetchsingleUser()
+    },[])
+    const fetchsingleUser = async()=>{
+        let res = await fetch(`http://localhost:3008/users/${id}`);
+        let result =  await res.json();
+        setsingleuser(result)
+    }
+    console.log(singleuser)
     return <div>
         hello {id}
     </div>
